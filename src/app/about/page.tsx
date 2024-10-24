09877fe1638553ad997fc5ad23561fcf92cc71f9
Mateus Guedes <mxteusgued@gmail.com>
2024-10-24 19:08:43 -0300

    create  about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,21 @@
-const About: React.ReactElement = () => {
+'use client'
+import { motion } from "framer-motion"
+import Link from "next/link"
+
+function About(): React.ReactElement {
     return(
-        <article>
-            <p>Text</p>
-        </article>
+        <div className="flex items-start  w-full h-24">
+            <div className="absolute left-1/2 transform -translate-x-1/2">
+                <Link href="/">
+                    <motion.p
+                        key="name"
+                        layoutId="name"
+                    >
+                        Mateus Guedes
+                    </motion.p>
+                </Link>
+            </div>
+        </div>
     )
 }
 
